5c86a92173be7f8a8923a7db0593bcefae63e198
Amit <amitband0704@gmail.com>
2026-08-11 22:38:12 +0530

    Updated ProgramDriver.java

--- DoublyLinkedList/ProgramDriver.java
+++ DoublyLinkedList/ProgramDriver.java
@@ -1,13 +1,25 @@
 public class ProgramDriver {
     	public static void main(String[] args) {
 		DoublyLinkedList dll = new DoublyLinkedList();
+		//! Adding elements into end of the doubly linked list
 		dll.addAtEnd(10);
 		dll.addAtEnd(20);
 		dll.addAtEnd(30);
 		dll.addAtEnd(40);
-		System.out.println(dll.head.data);
-		System.out.println(dll.head.next.data);
-		System.out.println(dll.head.next.next.data);
-		System.out.println(dll.head.next.next.next.data);
+		dll.print();
+
+		// ! Adding elements into the start of the doubly linked list
+		dll.addAtBegin(5);
+		dll.print();
+
+		// ! Forward Traversal
+		dll.print();
+		// ! Reverse Traversal
+		dll.printRev();
+
+		// Adding elements at a specific position(index) in the doubly linked list
+		dll.addAtIndex(3, 25);
+		dll.print();
+
 	}
 }
