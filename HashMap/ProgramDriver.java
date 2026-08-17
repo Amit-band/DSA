1ab9711758cd9f46d822478e4f3988f5749aec94
Amit <amitband0704@gmail.com>
2026-08-17 14:51:22 +0530

    Added Stack,Queue and HashMap

---
+++ HashMap/ProgramDriver.java
@@ -0,0 +1,24 @@
+public class ProgramDriver {
+	public static void main(String[] args) {
+		HashMap hm = new HashMap(5);
+		hm.put(101,"Allen");
+		hm.put(106, "Amit");
+		hm.put(111,"Kevin");
+		hm.put(103, "Eleven");
+		hm.put(106, "Massom");
+		hm.put(111, "Virat");
+		System.out.println(hm.buckets[3].key);
+		System.out.println(hm.buckets[3].value);
+		
+		System.out.println(hm.buckets[1].key);
+		System.out.println(hm.buckets[1].value);
+		
+		System.out.println(hm.buckets[1].next.key);
+		System.out.println(hm.buckets[1].next.value);
+		
+		System.out.println(hm.buckets[1].next.next.key);
+		System.out.println(hm.buckets[1].next.next.value);
+		
+
+	}
+}
