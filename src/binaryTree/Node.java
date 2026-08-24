397487b86ce63ae9773971afc305fc89007bd7f7
Amit <amitband0704@gmail.com>
2026-08-24 15:16:56 +0530

    Added Binary Tree

---
+++ src/binaryTree/Node.java
@@ -0,0 +1,11 @@
+package binaryTree;
+
+public class Node {
+	Node prev;
+	Node next;
+	int data;
+	
+	public Node(int data) {
+		this.data = data;
+	}
+}
---
+++ src/binaryTree/ProgramDriver.java
@@ -0,0 +1,12 @@
+package binaryTree;
+
+public class ProgramDriver {
+	public static void main(String[] args) {
+		BinaryTree bt = new BinaryTree();
+		bt.add(30);
+		bt.add(10);
+		bt.add(20);
+		bt.add(40);
+		bt.add(90);
+	}
+}
